9b0a4d6aa09c4e1d9701b8805b65ec2ea57ff5a8
Jochen Steinmann <jochen.steinmann@web.de>
2023-02-05 15:04:13 +0100

    updated addr pins
    since the MAX31865 is using the B port -> move address pins to C

--- firmware/config.h
+++ firmware/config.h
@@ -7,8 +7,8 @@
 
 #define LED0     D,2
 #define LED1     D,3
-#define ADDR0    B,0
-#define ADDR1    B,1
-#define ADDR2    B,2
+#define ADDR0    C,0
+#define ADDR1    C,1
+#define ADDR2    C,2
 
 #endif /* CONFIG_H */
