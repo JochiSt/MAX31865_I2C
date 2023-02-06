7d4a5b52e1ff8ce4e049e94c48a9f01d7cf158d2
Jochen Steinmann <jochen.steinmann@web.de>
2023-02-06 17:17:53 +0100

    set i2c_scan baudrate to 100

--- software/i2c_scan.py
+++ software/i2c_scan.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from i2c_mp_usb import I2C_MP_USB as SMBus
 bus = SMBus()
-#bus.set_baudrate(50)
+bus.set_baudrate(100)
 
 print("     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f")
 for i in range(0, 128, 16):
