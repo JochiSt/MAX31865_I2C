812b142b91c84a58a0c62acec6718644d3935de4
Jochen Steinmann <jochen.steinmann@web.de>
2023-02-07 15:15:51 +0100

    put MAX31865_I2C functions in class
    easier to use and much more structured

--- software/MAX31865_I2C.py
+++ software/MAX31865_I2C.py
@@ -1,32 +1,41 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb  6 14:54:02 2023
 
-@author: steinmann
 """
-import sys
+
+class MAX31865_I2C(object):
+    CNT_REG_ADDR = [0x00, 0x00]
+    RTD_REG_ADDR = [0x00, 0x01]
+
+    def __init__(self, i2c_bus, MAX31865_I2C_ADDR = 0x17):
+        self.bus = i2c_bus
+        self.addr = MAX31865_I2C_ADDR
+
+    def probe(self):
+        return self.bus.probe_device(self.addr)
+
+    def readRTD(self):
+        bus.write_i2c_block_raw(self.addr, MAX31865_I2C.RTD_REG_ADDR)
+        data = bus.read_i2c_block_raw(self.addr, 2)
+        return data[1] << 7 | data[0]
+
+    def readCounter(self):
+        bus.write_i2c_block_raw(self.addr, MAX31865_I2C.CNT_REG_ADDR)
+        data = bus.read_i2c_block_raw(self.addr, 1)
+        return int.from_bytes(data, 'big')
 
 from i2c_mp_usb import I2C_MP_USB as SMBus
 bus = SMBus()
-
-MAX31865_I2C_ADDR = 0x17
-
 bus.set_baudrate(100)
 
 try:
 
-    if bus.probe_device(MAX31865_I2C_ADDR):
+    max31865i2c = MAX31865_I2C(bus)
+
+    if max31865i2c.probe():
         print('device ready')
-
-        #bus.write_i2c_block_raw(MAX31865_I2C_ADDR, [0x00, 20, 1])
-        bus.write_i2c_block_raw(MAX31865_I2C_ADDR, [0x00, 0x00])
-
-        data = bus.read_i2c_block_raw(MAX31865_I2C_ADDR, 21)
-        #bus.write_byte(MAX31865_I2C_ADDR, 0x00)
-
-        # data = bus.read_byte(MAX31865_I2C_ADDR)
-        print( data )
-
+        print(max31865i2c.readCounter())
+        print(max31865i2c.readRTD())
     else:
         print('device not ready')
 except Exception as e:
