03d69fd459b91598a4b41075a4d628b3ed679ec5
Jochen Steinmann <jochen.steinmann@web.de>
2023-02-11 20:11:04 +0100

    added script to generate a pythonic register map from the C enum

---
+++ software/createRegisterMap.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+
+comment = False
+
+with open("../firmware/i2c_register_map.h") as reg_file:
+    with open("MAX31865_I2C_registermap.py", "w") as pyreg:
+        pyreg.write("from AutoNumber import AutoNumber\n")
+        pyreg.write("")
+
+        for line in reg_file:
+            line = line[:-1]        # strip \n
+            comment_pos = line.find("//")
+
+            if(comment_pos != -1):
+                line_comment = line[comment_pos:]
+                line = line[:comment_pos]
+            else:
+                line_comment = ''
+
+            if(len(line) < 1):
+                continue
+
+            if '#' == line[0]:      # exclude preprocessor statements
+                continue
+            if '//' == line[0:1]:   # exclude lines with starting comment
+                continue
+            if '/*' in line:
+                comment = True
+            if '*/' in line:
+                comment = False
+                continue
+
+            if comment:
+               continue
+
+            # here we only have lines, which we want to work with
+            if '}' in line:
+                continue
+
+            if 'enum' in line:
+                pyreg.write("class MAX31865_I2C_registermap(AutoNumber):\n")
+                continue
+
+            line = line.strip()
+            line = line.replace(',','')
+
+            print(line)
+            line_comment = line_comment.replace('///<','')
+            line_comment = line_comment.replace('/','')
+
+            if '=' in line:
+                pyreg.write("\t%s #%s\n"%(line, line_comment))
+            else:
+                pyreg.write("\t%s = () #%s\n"%(line, line_comment))
+
+
