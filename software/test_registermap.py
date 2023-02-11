1a439ee53a6cc343dcad452d3000bf598bfc3bb4
Jochen Steinmann <jochen.steinmann@web.de>
2023-02-11 20:45:55 +0100

    added small test for generated register map

---
+++ software/test_registermap.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 11 20:26:30 2023
+
+@author: steinmann
+"""
+
+from MAX31865_I2C_registermap import MAX31865_I2C_registermap
+
+print(MAX31865_I2C_registermap.I2C_FREQ_STATUS_CNT.value)
+print(MAX31865_I2C_registermap.I2C_MAX31865_RTD0.value)
