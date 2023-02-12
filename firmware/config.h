cdb48e4e883d96fbd56da93f0cf5f856abaa8126
Jochen Steinmann <jochen.steinmann@web.de>
2023-02-12 13:49:44 +0100

    updated pin mapping to match latest PCB revision

--- firmware/config.h
+++ firmware/config.h
@@ -5,11 +5,16 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define LED0     D,2
-#define LED1     D,3
-#define ADDR0    C,0
-#define ADDR1    C,1
-#define ADDR2    C,2
+#define LED0    D,5
+#define LED1    D,6
+#define ADDR0   D,2
+#define ADDR1   D,3
+#define ADDR2   D,4
+
+#define GPIO0   C,3
+#define GPIO1   C,1
+#define GPIO2   C,2
+#define GPIO3   C,0
 
 #define BAUDRATE 9600
 
