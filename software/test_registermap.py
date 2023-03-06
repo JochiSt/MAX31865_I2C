54015bf2562a194ff81d88b739d7c33deb981c54
Jochen Steinmann <jochen.steinmann@web.de>
2023-03-06 14:19:14 +0100

    updated register map test

--- software/test_registermap.py
+++ software/test_registermap.py
@@ -7,4 +7,7 @@ Created on Sat Feb 11 20:26:30 2023
 
 from MAX31865_I2C_registermap import MAX31865_I2C_registermap
 
-print(MAX31865_I2C_registermap.I2C_MAX31865_RTD0.value)print(MAX31865_I2C_registermap.I2C_MAX31865_RTD0.value)
+print(MAX31865_I2C_registermap.I2C_STATUS_CNT.value)
+print(MAX31865_I2C_registermap.I2C_MAX31865_RTD0.value)
+
+print( MAX31865_I2C_registermap.I2C_MAX31865_RTD0.value.to_bytes(2, 'big') )
